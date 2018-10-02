409a5c2c87b42dad773c7085858fe27482fc6dfa
Shayne van Asperen <shaynevanasperen@gmail.com>
2018-10-02 11:26:38 +0100

    Remove unused code

--- src/Kongverge/Helpers/HttpExtensions.cs
+++ src/Kongverge/Helpers/HttpExtensions.cs
@@ -1,8 +1,5 @@
-using System;
 using System.Net.Http;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace Kongverge.Helpers
 {
@@ -10,22 +7,5 @@ namespace Kongverge.Helpers
     {
         public static StringContent AsJsonStringContent(this string json) =>
             new StringContent(json, Encoding.UTF8, "application/json");
-
-        public static Task<HttpResponseMessage> PatchAsync(
-            this HttpClient client,
-            string requestUri,
-            HttpContent content,
-            CancellationToken cancellationToken = default) =>
-            client.PatchAsync(new Uri(requestUri, UriKind.RelativeOrAbsolute), content, cancellationToken);
-
-        public static Task<HttpResponseMessage> PatchAsync(
-            this HttpClient client,
-            Uri requestUri,
-            HttpContent content,
-            CancellationToken cancellationToken = default)
-        {
-            var request = new HttpRequestMessage(new HttpMethod("PATCH"), requestUri) { Content = content };
-            return client.SendAsync(request, cancellationToken);
-        }
     }
 }
